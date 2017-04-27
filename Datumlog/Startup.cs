8b2c714e80bb842453a171f33521eda82f95748c
Shakir Ullah <shakir.webit@gmail.com>
2017-04-27 09:13:53 +0500

    added new project

---
+++ Datumlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Datumlog.Startup))]
+namespace Datumlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
